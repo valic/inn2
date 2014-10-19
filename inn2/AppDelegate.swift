9e7d01fd00dc28d9e20291b7618a9479c0d03f72
Мялин Валентин <valic@iMac-Malin.local>
2014-10-20 02:20:35 +0300

    Initial Commit

---
+++ inn2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  inn2
+//
+//  Created by Мялин Валентин on 20.10.14.
+//  Copyright (c) 2014 mialin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ inn2Tests/inn2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  inn2Tests.swift
+//  inn2Tests
+//
+//  Created by Мялин Валентин on 20.10.14.
+//  Copyright (c) 2014 mialin. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class inn2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
